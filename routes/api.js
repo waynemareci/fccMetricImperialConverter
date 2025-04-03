e7aa210167dcfba5980137ff59e419fa8f1d6555
waynemareci <wayne@mareci.com>
2025-04-03 19:34:52 +0000

    tests 1, 2, 12 pass

--- routes/api.js
+++ routes/api.js
@@ -10,7 +10,11 @@ module.exports = function (app) {
 
   app.get('/api/convert',(req,res) => {
     console.log("api/convert req.query.input: " + req.query.input)
-    
+    var regex = /([a-zA-Z]+)$/;
+    const matchEnd = req.query.input.match(regex)
+    regex = /^([^a-zA_Z]+)/
+    const matchBeginning = req.query.input.match(regex)
+    console.log("Found " + matchBeginning[0] + " input value; " + matchEnd[0] + " input unit")
   })
 
 };
